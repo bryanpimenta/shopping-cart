59afb07041808d8e2605ebcd5839f6a0a7a44369
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-22 16:09:03 -0300

    Requisito 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,19 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+export const fetchProductsList = async (QUERY) => {
+  if (!QUERY) {
+    throw new Error('Termo de busca não informado');
+  }
+  const LINK = `https://api.mercadolibre.com/sites/MLB/search?q=${QUERY}`;
+  try {
+    const response = await fetch(LINK);
+    const data = await response.json();
+    const dados = data;
+    const { results } = dados;
+
+    return results;
+  } catch (error) {
+    console.error(error);
+  }
 };

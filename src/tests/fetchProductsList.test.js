b890aa7b28b9817ddbd003ce58158ae07e262d64
Bryan Pimenta <bryanpimenta30@gmail.com>
2023-03-22 15:36:01 -0300

    Requisito 1

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,29 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toEqual('function');
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    const endpoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledWith(endpoint);
   });
-
+  
+  it('Testando o retorno é uma estrutura de dados em objeto da função fetchProductsList', async () => {
+    const response = await fetchProductsList('computador');
+    expect(response).toHaveBeenCalledWith(computadorSearch);
+  });
+  
+  it ('Testando a função fetchProductsList sem parametro', async () => {
+    const mensage = await fetchProductsList();
+    expect(mensage).toThrow();
+  });
   // it('...', () => {
   // });
 });

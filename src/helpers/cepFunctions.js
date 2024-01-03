c8f6038586306d76566d6c4e8c9d5c4bae4c21c4
Bryan Pimenta <bryanpimenta30@gmail.com>
2024-01-03 18:06:40 -0300

    (feat) requisito 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -1,7 +1,27 @@
-export const getAddress = () => {
-  // seu código aqui
+export const getAddress = async () => {
+  const inputCep = document.querySelector('section.cart input.cep-input');
+  const API_1 = `https://cep.awesomeapi.com.br/json/${inputCep.value}`;
+  const API_2 = `https://brasilapi.com.br/api/cep/v2/${inputCep.value}`;
+  let infos;
+
+  await Promise.any([fetch(API_1), fetch(API_2)])
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.cep) {
+        infos = `${data.address} - ${data.district} - ${data.city} - ${data.state}`
+      || `${data.street} - ${data.neighborhood} - ${data.city} - ${data.state}`;
+      } else throw new Error('CEP não encontrado');
+    });
+  return infos;
 };
 
-export const searchCep = () => {
-  // seu código aqui
+export const searchCep = async () => {
+  try {
+    const data = await getAddress();
+    const tagEndereco = document.querySelector('.cart__address');
+    tagEndereco.innerHTML = data;
+  } catch (error) {
+    const tagEndereco = document.querySelector('.cart__address');
+    tagEndereco.innerHTML = 'CEP não encontrado';
+  }
 };
